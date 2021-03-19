f1a11abf42af327e54c9b01d92b904a209d28c9d
leonardo <leo_zinho_filho@hotmail.com>
2021-03-19 10:24:09 -0300

    Subindo Frontend

---
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -0,0 +1,40 @@
+import { Product } from './../product.model';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: './product-create.component.html',
+  styleUrls: ['./product-create.component.css']
+})
+export class ProductCreateComponent implements OnInit {
+
+  product: Product = {
+    name:'',
+    price: null
+  }
+
+  constructor(private productService: ProductService,
+    private router: Router) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  createProduct():void{
+    this.productService.create(this.product).subscribe(()=>{
+      this.productService.showMessage('Produto Criado')
+      this.router.navigate(['/usuarios'])
+    })
+
+    
+
+  }
+  cancel():void{
+    this.router.navigate(['/usuarios'])
+
+  }
+
+
+}
